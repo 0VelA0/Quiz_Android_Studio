42754a1fd5a576756cbb558dc466fd9cbcb15011
Angel Gabriel Herrera Contreras <aghcx3@outlook.com>
2024-03-06 15:27:10 -0600

    Añadi el Spinner

--- a/app/src/main/java/com/example/myapplication/Activity1.kt
+++ a/app/src/main/java/com/example/myapplication/Activity1.kt
@@ -4,20 +4,30 @@ import android.content.Intent
 import android.media.Image
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class Activity1 : AppCompatActivity() {
     private lateinit var image: Image
     private lateinit var jugar: Button
     private lateinit var opciones: Button
+    private lateinit var dificultad: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_1)
 
+        val items = listOf("Facil", "Normal", "Dificil")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
         jugar = findViewById(R.id.jugar)
         opciones = findViewById(R.id.Opciones)
+        dificultad = findViewById(R.id.spinner_dificultad)
 
         jugar.setOnClickListener {
             val intent = Intent(this,MainActivity::class.java)
@@ -27,6 +37,26 @@ class Activity1 : AppCompatActivity() {
             val intent = Intent(this,Activity2::class.java)
             startActivity(intent)
         }
+
+        dificultad.adapter = adapter
+
+        dificultad.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = items[position]
+                //Mandar el item seleccionado como dificultad
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //Mandar por default la dificultad normal
+            }
+        }
+
+
     }
 
 
