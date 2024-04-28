9694cc58e9a6fa199bcb893e5c98a5768b6acf63
Angel Sanchez <ctm.angeledu369@gmail.com>
2024-04-28 13:32:39 -0600

    limpie codigo

--- a/app/src/main/java/com/example/myapplication/DataBase.kt
+++ a/app/src/main/java/com/example/myapplication/DataBase.kt
@@ -2,10 +2,9 @@ package com.example.myapplication
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.myapplication.UserDao
 
 @Database(entities = [User::class,Preguntas::class,GameSave::class], version = 1)
-abstract class bd : RoomDatabase() {
+abstract class DataBase : RoomDatabase() {
     abstract fun UserDao():UserDao
     abstract fun PreguntasDao():PreguntasDao
     abstract fun GameSaveDao():GameSaveDao
